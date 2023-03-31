a5e54fc213cd8a881c6d0abac902b1fbe671cc38
Alexandr Popov <He1ton@yandex.ru>
2023-03-31 18:19:57 +0300

    Push project

---
+++ SceneController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private int _indexScene;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(_indexScene);
+    }
+  
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //public void BringBackTheScene()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene())
+    //}
+}
